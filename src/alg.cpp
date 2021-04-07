b16b2cbcb39b9399d9392e3ce8d85d8e6e2c106b
MokerovaE <79098309+MokerovaE@users.noreply.github.com>
2021-04-07 16:11:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,56 +1,54 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++)
-    for (int j = i + 1; j < len; j++)
+  int number = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value)
-        count++;
-  return count;
+        number++;
+    }
+  }
+  return number;
 }
 
 int countPairs2(int *arr, int len, int value) {
-  int count = 0, x = 0, y = len - 1;
-  while (x < y) {
-    int mid = (x + y) / 2;
-    if (value == arr[mid])
-      break;
-    else if (value > arr[mid])
-      x = mid;
+  int number = 0, L = 0, R = len - 1, midde;
+  while (L < R) {
+    middle = (L + R) / 2;
+    if (arr[middle] < value)
+      L = mid;
     else
-      y = mid;
+      R = mid;
   }
-  for (int i = y; i >= 0; i--)
+  for (int i = R; i >= 0; i--)
     for (int j = 0; j < i; j++)
       if (arr[i] + arr[j] == value)
-        count++;
-      else if (arr[i] + arr[j] > value)
-        break;
-  return count;
+        number++;
+  return number;
 }
 
 int countPairs3(int *arr, int len, int value) {
-  int count = 0, left = 0, right = len - 1;
-  while (left < right-1) {
-    int middle = (left + right) / 2;
+  int number = 0, L = 0, R = len - 1;
+  while (L < R - 1) {
+    int middle = (L + R) / 2;
     if (arr[middle] <= value)
-      left = middle;
+      L = middle;
     else
-      right = middle;
+      R = middle;
   }
-  for (int i = 0; i < left; i++) {
-  int x = i + 1, y = left;
-    while (x < y) {
-      int mid = (x + y) / 2;
-      if (arr[mid] < value - arr[i])
-        x = mid + 1;
+  for (int i = 0; i < L; i++) {
+  int left = i + 1, right = L, mid;
+    while (left < right) {
+      mid = (left + right) / 2;
+      if (arr[mid] < (value - arr[i]))
+        left = mid + 1;
       else
-        y = mid;
+        right = mid;
     }
-    while (arr[x] == value - arr[i]) {
-      count++;
-      x++;
+    while (arr[left] == (value - arr[i])) {
+      number++;
+      left++;
     }
   }
-  return count;
+  return number;
 }
